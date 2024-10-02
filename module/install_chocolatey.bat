54db64a5ed56d20f7657a2183824f6b7551ceee8
Muhd Amri <m-6104282@moe-dl.edu.my>
2024-10-02 23:23:52 +0800

    Update install_chocolatey.bat

--- module/install_chocolatey.bat
+++ module/install_chocolatey.bat
@@ -2,9 +2,8 @@
 
 if %errorlevel%==1 (
     echo Installing Chocolatey...
-    start /wait cmd /c "@powershell -NoProfile -ExecutionPolicy Bypass -Command \"Set-ExecutionPolicy Bypass -Scope Process; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))\""
-    start cmd /c refreshenv
-    echo Chocolatey installed and environment variables refreshed.
+    @powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+    refreshenv
 ) else (
     echo Chocolatey is already installed.
 )
