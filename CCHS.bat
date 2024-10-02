c4001e703d8dcacc25eee76b9a1e5c79bf289617
Muhd Amri <m-6104282@moe-dl.edu.my>
2024-10-02 22:46:38 +0800

    Update CCHS.bat

--- CCHS.bat
+++ CCHS.bat
@@ -88,13 +88,14 @@ if %runTimeSync%==1 (
     pause
 )
 
-:: Chocolatey installation check
+:: Check if Chocolatey is installed
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "if ((Get-Command -Name choco.exe -ErrorAction SilentlyContinue) -eq $null) { exit 1 } else { exit 0 }"
 
 if %errorlevel%==1 (
     echo Installing Chocolatey...
-    @powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
-    refreshenv
+    start /wait cmd /c "@powershell -NoProfile -ExecutionPolicy Bypass -Command \"Set-ExecutionPolicy Bypass -Scope Process; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))\""
+    start cmd /c refreshenv
+    echo Chocolatey installed and environment variables refreshed.
 ) else (
     echo Chocolatey is already installed.
 )
