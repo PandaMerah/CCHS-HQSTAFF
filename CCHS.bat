db837e2a26de8842cb3730d19ce785b6742cd9dd
Muhd Amri <m-6104282@moe-dl.edu.my>
2024-10-02 21:55:38 +0800

    Update CCHS.bat

--- CCHS.bat
+++ CCHS.bat
@@ -13,6 +13,9 @@ set installPrinters=0
 set installOffice=0
 set installTPP=0
 set addHQWifi=0
+set runTimeSync=0
+set runChocolatey=0
+set installApps=0
 set SSID=CCHS
 set HEXSTR=125cchsHQ2023
 set XML_OUTPUT_PATH=%TEMP%\%SSID%-wireless-profile-generated.xml
@@ -25,10 +28,25 @@ set printer2_name=Sharp MX-3140n Printer
 echo Customization of the installation script
 cd "%USERPROFILE%\Downloads\CCHS-HQSTAFF-main"
 
-:: User Customization
-choice /M "Install printers HQ?"
+:: User Customization: Choose modules to run
+choice /M "Add HQ WiFi?"
 if %errorlevel%==1 (
-    set installPrinters=1
+    set addHQWifi=1
+)
+
+choice /M "Sync Time and Set Timezone to Malaysia?"
+if %errorlevel%==1 (
+    set runTimeSync=1
+)
+
+choice /M "Install Chocolatey?"
+if %errorlevel%==1 (
+    set runChocolatey=1
+)
+
+choice /M "Install Apps (e.g., Chrome, AnyDesk, Teams)?"
+if %errorlevel%==1 (
+    set installApps=1
 )
 
 choice /M "Install Office or WPS?"
@@ -43,95 +61,76 @@ if %errorlevel%==1 (
     set installTPP=1
 )
 
-choice /M "Add HQ Wifi?"
+choice /M "Install HQ Printers?"
 if %errorlevel%==1 (
-    set addHQWifi=1
+    set installPrinters=1
 )
 
-:: Adding HQ Wifi to the list
+:: Add HQ Wifi to the list
 if %addHQWifi%==1 (
-    echo Adding HQ Wifi
+    echo Adding HQ WiFi
     echo ^<?xml version="1.0"?^>^<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1"^>^<name^>%SSID%^</name^>^<SSIDConfig^>^<SSID^>^<name^>%SSID%^</name^>^</SSID^>^</SSIDConfig^>^<connectionType^>ESS^</connectionType^>^<connectionMode^>auto^</connectionMode^>^<MSM^>^<security^>^<authEncryption^>^<authentication^>WPA2PSK^</authentication^>^<encryption^>AES^</encryption^>^<useOneX^>false^</useOneX^>^</authEncryption^>^<sharedKey^>^<keyType^>passPhrase^</keyType^>^<protected^>false^</protected^>^<keyMaterial^>%HEXSTR%^</keyMaterial^>^</sharedKey^>^</security^>^</MSM^>^<MacRandomization xmlns="http://www.microsoft.com/networking/WLAN/profile/v3"^>^<enableRandomization^>false^</enableRandomization^>^</MacRandomization^>^</WLANProfile^> >%XML_OUTPUT_PATH%
     netsh wlan add profile filename="%XML_OUTPUT_PATH%"
     netsh wlan connect name="%SSID%"
     del "%XML_OUTPUT_PATH%"
 )
 
-:: Set timezone to Malaysia and sync the time
-sc triggerinfo w32time delete
-tzutil /s "Singapore Standard Time"
-w32tm /resync
-echo The current time and date:
-date /t
-time /t
-echo Time has been synced, please check if it's correct
-pause
-
-:: Check if Chocolatey is installed
-choco -v >nul 2>&1
-if %errorlevel% neq 0 (
-    echo ........Installing Chocolatey........
-    @powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
-    :: Refresh environment variables after installing Chocolatey
-    refreshenv
-) else (
-    echo ........Chocolatey is already installed, skipping installation........
+:: Sync time and set timezone
+if %runTimeSync%==1 (
+    echo Setting Timezone to Malaysia and syncing time...
+    sc triggerinfo w32time delete
+    tzutil /s "Singapore Standard Time"
+    w32tm /resync
+    echo The current time and date:
+    date /t
+    time /t
+    echo Time has been synced, please check if it's correct
+    pause
 )
 
-:: Install applications using Chocolatey
-choco install googlechrome -y --force
-choco install anydesk.install -y --force
-choco install microsoft-teams -y --force
-choco install winrar -y --force
-choco install box-sync -y --force
-choco install adobereader -y --force
+:: Chocolatey installation check
+if %runChocolatey%==1 (
+    choco -v >nul 2>&1
+    if %errorlevel% neq 0 (
+        echo Installing Chocolatey...
+        @powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+        :: Refresh environment variables after installing Chocolatey
+        refreshenv
+    ) else (
+        echo Chocolatey is already installed, skipping installation...
+    )
+)
 
-echo ........All apps installed successfully........
+:: Install apps using Chocolatey
+if %installApps%==1 (
+    echo Installing applications...
+    choco install googlechrome -y --force
+    choco install anydesk.install -y --force
+    choco install microsoft-teams -y --force
+    choco install winrar -y --force
+    choco install box-sync -y --force
+    choco install adobereader -y --force
+    echo All apps installed successfully.
+)
 
 :: Install Office or WPS
 if %installOffice%==1 (
-    echo ........Installing Office........
+    echo Installing Office...
     start .\Office\setup.exe /configure .\Office\configuration.xml
 ) else (
-    echo ........Installing WPS........
+    echo Installing WPS...
     choco install wps-office -y --force
 )
 
 :: Install TPP
 if %installTPP%==1 (
-    echo ........Installing TPP........
+    echo Installing TPP...
     start /w .\TPP.exe /silent
 )
 
-:: Customizing Windows
-echo ........Customizing Windows........
-
-:: Turn off Windows Copilot
-reg add "HKCU\Software\Policies\Microsoft\Windows\WindowsCopilot" /v TurnOffWindowsCopilot /t REG_DWORD /d 1 /f
-
-:: Hide Task View button
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton /t REG_DWORD /d 0 /f
-
-:: Disable Widgets on Taskbar
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarDa /t REG_DWORD /d 0 /f
-
-:: Show search box on Taskbar
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 1 /f
-
-:: Copy wallpaper to user's Documents folder
-copy /Y ".\CCHS_WALLPAPER.jpg" "%USERPROFILE%\Documents"
-
-:: Set custom wallpaper
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%USERPROFILE%\Documents\CCHS_WALLPAPER.jpg" /f
-
-:: Refresh the wallpaper setting
-RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters ,1 ,True
-
-echo ........Windows customization complete........
-
 :: Install HQ Printers
 if %installPrinters%==1 (
-    echo ........Installing HQ Printers........
+    echo Installing HQ Printers...
 
     :: Install Konica Printer Driver
     pnputil /add-driver .\printerDriver\Konica\KOAXCJ__.inf /install
