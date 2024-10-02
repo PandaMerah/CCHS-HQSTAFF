9df2969d2b36fd955494d8dfeefee6885567c8d8
Muhd Amri <m-6104282@moe-dl.edu.my>
2024-10-02 15:23:08 +0800

    Update CCHS.bat

--- CCHS.bat
+++ CCHS.bat
@@ -1,6 +1,14 @@
 @echo off
 
-::Variable Declaration
+:: Check if running as Administrator
+openfiles >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+:: Variable Declaration
 set installPrinters=0
 set installOffice=0
 set installTPP=0
@@ -9,7 +17,7 @@ set SSID=CCHS
 set HEXSTR=125cchsHQ2023
 set XML_OUTPUT_PATH=%TEMP%\%SSID%-wireless-profile-generated.xml
 set printer1_ip=192.168.1.150
-set printer2_name=Konica Minolta C226i Printer
+set printer1_name=Konica Minolta C226i Printer
 set printer2_ip=192.168.1.180
 set printer2_name=Sharp MX-3140n Printer
 
@@ -22,51 +30,55 @@ choice /M "Install printers HQ?"
 if %errorlevel%==1 (
     set installPrinters=1
 )
+
 choice /M "Install Office or WPS?"
 if %errorlevel%==1 (
     set installOffice=1
 ) else (
     set installOffice=0
 )
+
 choice /M "Install TPP?"
 if %errorlevel%==1 (
     set installTPP=1
 )
-choice /M "Install addHQWifi?"
+
+choice /M "Add HQ Wifi?"
 if %errorlevel%==1 (
     set addHQWifi=1
 )
 
-::Adding HQ Wifi to the list
+:: Adding HQ Wifi to the list
 if %addHQWifi%==1 (
-  echo Adding HQ Wifi
-  echo ^<?xml version="1.0"?^>^<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1"^>^<name^>%SSID%^</name^>^<SSIDConfig^>^<SSID^>^<name^>%SSID%^</name^>^</SSID^>^</SSIDConfig^>^<connectionType^>ESS^</connectionType^>^<connectionMode^>auto^</connectionMode^>^<MSM^>^<security^>^<authEncryption^>^<authentication^>WPA2PSK^</authentication^>^<encryption^>AES^</encryption^>^<useOneX^>false^</useOneX^>^</authEncryption^>^<sharedKey^>^<keyType^>passPhrase^</keyType^>^<protected^>false^</protected^>^<keyMaterial^>%HEXSTR%^</keyMaterial^>^</sharedKey^>^</security^>^</MSM^>^<MacRandomization xmlns="http://www.microsoft.com/networking/WLAN/profile/v3"^>^<enableRandomization^>false^</enableRandomization^>^</MacRandomization^>^</WLANProfile^> >%XML_OUTPUT_PATH%
-  netsh wlan add profile filename="%XML_OUTPUT_PATH%"
-  netsh wlan connect name="%SSID%"
-  del "%XML_OUTPUT_PATH%"
+    echo Adding HQ Wifi
+    echo ^<?xml version="1.0"?^>^<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1"^>^<name^>%SSID%^</name^>^<SSIDConfig^>^<SSID^>^<name^>%SSID%^</name^>^</SSID^>^</SSIDConfig^>^<connectionType^>ESS^</connectionType^>^<connectionMode^>auto^</connectionMode^>^<MSM^>^<security^>^<authEncryption^>^<authentication^>WPA2PSK^</authentication^>^<encryption^>AES^</encryption^>^<useOneX^>false^</useOneX^>^</authEncryption^>^<sharedKey^>^<keyType^>passPhrase^</keyType^>^<protected^>false^</protected^>^<keyMaterial^>%HEXSTR%^</keyMaterial^>^</sharedKey^>^</security^>^</MSM^>^<MacRandomization xmlns="http://www.microsoft.com/networking/WLAN/profile/v3"^>^<enableRandomization^>false^</enableRandomization^>^</MacRandomization^>^</WLANProfile^> >%XML_OUTPUT_PATH%
+    netsh wlan add profile filename="%XML_OUTPUT_PATH%"
+    netsh wlan connect name="%SSID%"
+    del "%XML_OUTPUT_PATH%"
 )
 
-::Set timezone to Malaysia and sync the time
+:: Set timezone to Malaysia and sync the time
 sc triggerinfo w32time delete
 tzutil /s "Singapore Standard Time"
 w32tm /resync
 echo The current time and date:
 date /t
 time /t
-echo Time has been sync, please check the time is it correct
+echo Time has been synced, please check if it's correct
 pause
 
-:: Installing Apps
-echo ........Installing Chocolatey........
-
-:: Install Chocolatey
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
-
-:: Refresh environment variables
-refreshenv
+:: Check if Chocolatey is installed
+choco -v >nul 2>&1
+if %errorlevel% neq 0 (
+    echo ........Installing Chocolatey........
+    @powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+    :: Refresh environment variables after installing Chocolatey
+    refreshenv
+) else (
+    echo ........Chocolatey is already installed, skipping installation........
+)
 
 :: Install applications using Chocolatey
-choco upgrade chocolatey
 choco install googlechrome -y --force
 choco install anydesk.install -y --force
 choco install microsoft-teams -y --force
@@ -76,16 +88,16 @@ choco install adobereader -y --force
 
 echo ........All apps installed successfully........
 
-
+:: Install Office or WPS
 if %installOffice%==1 (
     echo ........Installing Office........
-    ::call .\Office\office.bat
     start .\Office\setup.exe /configure .\Office\configuration.xml
-) else if %installOffice%==0 (
+) else (
     echo ........Installing WPS........
     choco install wps-office -y --force
 )
 
+:: Install TPP
 if %installTPP%==1 (
     echo ........Installing TPP........
     start /w .\TPP.exe /silent
@@ -117,9 +129,7 @@ RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters ,1 ,True
 
 echo ........Windows customization complete........
 
-
-
-:: Installing HQ Printers
+:: Install HQ Printers
 if %installPrinters%==1 (
     echo ........Installing HQ Printers........
 
